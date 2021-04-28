4bad474f4000aa20fd40b8b029544b98b283ecf0
Prakhar Singh <prakhars76@gmail.com>
2021-04-28 15:06:41 +0530

    refactored products component

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -1,45 +1,54 @@
-import classes from './AvailableProducts.module.css';
+import Card from "../UI/Card";
+import classes from "./AvailableProducts.module.css";
+import ProductItem from "./ProductItem/ProductItem";
 
 const DUMMY_PRODUCTS = [
-    {
-        id : 'p1',
-        name: 'Masks',
-        description: 'good quality masks',
-        price: 30,
-    },
+  {
+    id: "p1",
+    name: "Masks",
+    description: "good quality masks",
+    price: 30,
+  },
 
-    {
-        id : 'p2',
-        name: 'Hand sanitizer',
-        description: 'It contains alchohol use carefully',
-        price: 50,
-    },
+  {
+    id: "p2",
+    name: "Hand sanitizer",
+    description: "It contains alchohol use carefully",
+    price: 50,
+  },
 
-    {
-        id : 'p3',
-        name: 'Water Bottle',
-        description: 'bottle for water',
-        price: 100,
-    },
+  {
+    id: "p3",
+    name: "Water Bottle",
+    description: "bottle for water",
+    price: 100,
+  },
 
-    {
-        id : 'p4',
-        name: 'guitar strings',
-        description: 'good quality strings',
-        price: 300,
-    },
-
-]
+  {
+    id: "p4",
+    name: "guitar strings",
+    description: "good quality strings",
+    price: 300,
+  },
+];
 
 const AvailableProducts = () => {
+  const ProductList = DUMMY_PRODUCTS.map((product) => (
+    <ProductItem
+      key={product.id}
+      name={product.name}
+      description={product.description}
+      price={product.price}
+    />
+  ));
 
-    const ProductList = DUMMY_PRODUCTS.map((product) => <li>{product.name}</li>);
-
-    return <section className={classes.products}>
-        <ul>
-            {ProductList}
-        </ul>
+  return (
+    <section className={classes.products}>
+      <Card>
+        <ul>{ProductList}</ul>
+      </Card>
     </section>
-}
+  );
+};
 
 export default AvailableProducts;
